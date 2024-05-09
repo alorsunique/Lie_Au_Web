96168ddd73ade616e3164ce4d62895e0fcf321eb
alorsunique <sounique7800@gmail.com>
2024-05-09 15:15:05 +0800

    2024 May 09

--- google_images_scrape.py
+++ google_images_scrape.py
@@ -1,8 +1,11 @@
+import io
 import os
 import time
+from datetime import datetime
 from pathlib import Path
 
-import bs4
+import requests
+from PIL import Image
 
 script_path = Path(__file__).resolve()
 project_dir = script_path.parent
@@ -11,80 +14,96 @@ os.chdir(project_dir)
 with open("Resources_Path.txt", "r") as resources_text:
     resources_dir = Path(str(resources_text.readline()).replace('"', ''))
 
-print(resources_dir)
-
 from selenium import webdriver
 
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
+def get_image_url(driver, search_query, thumbnail_selector, image_selector, max_url_count):
+    image_url_list = set()
 
+    search_url = f"https://www.google.com/search?q={search_query}&tbm=isch"
+    driver.get(search_url)
 
+    input_pause = input(f"Input anything to resume: ")
 
-search_query = input("Enter your Google Images search query: ")
-search_url = f"https://www.google.com/search?q={search_query}&tbm=isch"
+    display_thumbnails = driver.find_elements(By.CLASS_NAME, thumbnail_selector)
+    print(f"Elements Found: {len(display_thumbnails)}")
 
+    url_count = 0
 
-driver_options = Options()
-driver_options.add_argument("--start-maximized")
-driver = webdriver.Chrome(options=driver_options)
-driver.get(search_url)
+    for thumbnail in display_thumbnails:
+        print(f"Thumbnail: {thumbnail}")
 
-input_pause = input(f"Input anything to resume: ")
+        try:
+            thumbnail.click()
+            time.sleep(4)
+        except:
+            continue
 
-image_url_list = set()
+        image_container = driver.find_elements(By.CSS_SELECTOR, image_selector)
 
-page_html = driver.page_source
-pageSoup = bs4.BeautifulSoup(page_html, 'html.parser')
-containers = pageSoup.findAll('div', {'class': "H8Rx8c"})
+        print(f"Image: {image_container}")
 
-print(len(containers))
+        for content in image_container:
 
-display_thumbnails = driver.find_elements(By.CLASS_NAME, "H8Rx8c")
+            if content.get_attribute('src') and 'http' in content.get_attribute('src'):
+                url_count += 1
+                print(f"Found Image. Count: {url_count}")
+                image_url_list.add(content.get_attribute('src'))
 
-print(len(display_thumbnails))
+            if len(image_url_list) >= max_url_count:
+                break
 
-max_url_count = 10
-
-for thumbnail in display_thumbnails:
-    print(f"Thumbnail: {thumbnail}")
-
-    try:
-        thumbnail.click()
-        time.sleep(4)
-    except:
-        continue
-
-    #image_container = driver.find_elements(By.CLASS_NAME, "sFlh5c pT0Scc iPVvYb")
-    #page_html = driver.page_source
-    #pageSoup = bs4.BeautifulSoup(page_html, 'html.parser')
-    #image_container = pageSoup.findAll('div', {'class': "p7sI2 PUxBg"})
-
-    #print(pageSoup)
-
-    #print(len(image_container))
-    #print(type(image_container))
-    # Sva75c > div.A8mJGd.NDuZHe.OGftbe-N7Eqid-H9tDt > div.LrPjRb > div.AQyBn > div.tvh9oe.BIB1wf > c-wiz > div > div > div > div > div.v6bUne > div.p7sI2.PUxBg > a > img.sFlh5c.pT0Scc.iPVvYb
-    image_container = driver.find_elements(By.CSS_SELECTOR, ".sFlh5c.pT0Scc.iPVvYb")
-
-    print(image_container)
-
-    for content in image_container:
-
-        if content.get_attribute('src') and 'http' in content.get_attribute('src'):
-            print(f"Found Image")
-            image_url_list.add(content.get_attribute('src'))
+        time.sleep(2)
 
         if len(image_url_list) >= max_url_count:
             break
 
+    return (image_url_list)
 
 
-    time.sleep(2)
+def download_images(image_url_list, download_dir):
+    count = 0
+    for entry in image_url_list:
+        count += 1
 
-    if len(image_url_list) >= max_url_count:
-        break
+        try:
+            image_content = requests.get(entry).content
+            image_file = io.BytesIO(image_content)
+            image = Image.open(image_file)
 
-print(image_url_list)
+            file_path = download_dir / f"image_{count}.jpg"
 
+            with open(file_path, "wb") as f:
+                image.save(f, "JPEG")
+
+            print("Success")
+        except Exception as e:
+            print('FAILED -', e)
+
+
+search_query = input("Enter your Google Images search query: ")
+
+current_time = time.time()
+start_time = current_time
+start_datetime = datetime.fromtimestamp(start_time)
+
+start_datetime_formatted = start_datetime.strftime("%Y%m%d_%H%M%S")
+
+session_dir = resources_dir / "Google Images Downloads" / start_datetime_formatted
+if not session_dir.exists():
+    os.makedirs(session_dir)
+
+thumbnail_selector = "H8Rx8c"
+image_selector = ".sFlh5c.pT0Scc.iPVvYb"
+
+driver_options = Options()
+driver_options.add_argument("--start-maximized")
+driver = webdriver.Chrome(options=driver_options)
+
+max_url_count = 20
+
+url_list = get_image_url(driver, search_query, thumbnail_selector, image_selector, max_url_count)
+download_images(url_list, session_dir)
