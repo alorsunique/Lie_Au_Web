c655c7164ef728b5e081334ac69f0935deb3b3a8
alorsunique <sounique7800@gmail.com>
2024-08-03 14:15:21 +0800

    2024 August 3

--- beibusosu_group.py
+++ beibusosu_group.py
@@ -1,109 +1,42 @@
 import os
-import time
 from pathlib import Path
-from urllib.parse import urlparse
 
 import requests
-from PIL import Image
 from bs4 import BeautifulSoup
 
-import os
-from datetime import datetime, timedelta
-from pathlib import Path
-from urllib.parse import urlparse
-import time
+import beibusosu_single
 
-import requests
-from PIL import Image
-from bs4 import BeautifulSoup
 
-def inner_link_download(url,resources_dir):
+def acquire_card_links(url):
     request_result = requests.get(url)  # Request the url
     soup = BeautifulSoup(request_result.text, 'html.parser')
 
-    left_top_box = soup.findAll('li', class_='aside-setting__views-item')
-    publish_date = left_top_box[1].text.strip()
-    publish_date_object = datetime.strptime(publish_date, "%Y-%m-%d")
+    card_soup = soup.findAll('a', class_='main-content__card-link')
 
-    left_middle_box = soup.find_all('div', class_='aside-setting__chapter')
-    model_soup = left_middle_box[-1]
+    post_link_list = []
 
-    model_list = []
-    found_model_soup = model_soup.find_all('a', class_='aside-setting__models-link')
-    for potential_model in found_model_soup:
-        model_list.append(potential_model.text.strip())
+    for content in card_soup:
+        print(f"Link found: {content.get('href')}")
+        post_link_list.append(content.get('href'))
 
-    main_image_box = soup.find_all('div', class_='box-massage')[0]
-    image_link_soup = main_image_box.find_all('a', class_='box-massage__card-link slideshowGalleryImage')
-
-    image_link_list = []
-    for link in image_link_soup:
-        image_link_list.append(link.get('href'))
-
-    count = 0
-    for image_link in image_link_list:
-        time.sleep(2.5)
-
-        to_add_mod_time = publish_date_object + count * timedelta(seconds=1)
-        string_time = to_add_mod_time.strftime("%Y%m%d_%H%M%S")
-
-        parsed_url = urlparse(image_link).path.replace("/", '')
-
-        try:
-            image = Image.open(requests.get(image_link, stream=True).raw)
-
-            for model in model_list:
-
-                model_dir = resources_dir / model
-
-                if not model_dir.exists():
-                    os.makedirs(model_dir)
-
-                output_name = f"{model}_{string_time}_{parsed_url}"
-                image_path = model_dir / output_name
-
-                if image.mode != 'RGB':
-                    image = image.convert('RGB')
-
-                if not image_path.exists():
-                    image.save(image_path, format='JPEG', quality=100)
-
-                else:
-                    print(f"Skipping")
-
-                os.utime(image_path, (to_add_mod_time.timestamp(), to_add_mod_time.timestamp()))
-
-        except requests.exceptions.RequestException as error:
-            print(f"Cannot Fetch: {image_link}: {error}")
-
-        except Exception as error:
-            print(f"Cannot Process: {image_link}: {error}")
-
-        count += 1
+    return post_link_list
 
 
-script_path = Path(__file__).resolve()
-project_dir = script_path.parent
-os.chdir(project_dir)
+if __name__ == "__main__":
+    script_path = Path(__file__).resolve()
+    project_dir = script_path.parent
 
-with open("beibusosu_resource_path.txt", "r") as beibosusu_resources_text:
-    beibosusu_resources_dir = Path(str(beibosusu_resources_text.readline()).replace('"', ''))
+    print(f"Project directory: {project_dir}")
 
+    os.chdir(project_dir)
 
-url = str(input("URL: "))
+    with open("beibusosu_resource_path.txt", "r") as beibosusu_resources_text:
+        beibosusu_resources_dir = Path(str(beibosusu_resources_text.readline()).replace('"', ''))
 
-request_result = requests.get(url)  # Request the url
-soup = BeautifulSoup(request_result.text, 'html.parser')
+    url = str(input("URL: "))
 
-card_soup = soup.findAll('a', class_='main-content__card-link')
+    post_link_list = acquire_card_links(url)
 
-post_link_list = []
-
-for content in card_soup:
-    print(f"Link found: {content.get('href')}")
-    post_link_list.append(content.get('href'))
-
-for link in post_link_list:
-    print(f"Working {link}")
-    inner_link_download(link, beibosusu_resources_dir)
-
+    for link in post_link_list:
+        print(f"Working {link}")
+        beibusosu_single.main_image_box_download(link, beibosusu_resources_dir)
--- beibusosu_single.py
+++ beibusosu_single.py
@@ -1,81 +1,108 @@
 import os
+import time
 from datetime import datetime, timedelta
 from pathlib import Path
 from urllib.parse import urlparse
-import time
 
 import requests
 from PIL import Image
 from bs4 import BeautifulSoup
 
-script_path = Path(__file__).resolve()
-project_dir = script_path.parent
-os.chdir(project_dir)
 
-with open("beibusosu_resource_path.txt", "r") as beibusosu_resources_text:
-    beibusosu_resources_dir = Path(str(beibusosu_resources_text.readline()).replace('"', ''))
+def main_image_box_download(url, resources_dir):
+    request_result = requests.get(url)  # Request the url
+    soup = BeautifulSoup(request_result.text, 'html.parser')
 
-url = str(input("URL: "))
+    left_top_box = soup.findAll('li', class_='aside-setting__views-item')
+    publish_date = left_top_box[1].text.strip()
+    print(f"Publish Date: {publish_date}")
+    publish_date_object = datetime.strptime(publish_date, "%Y-%m-%d")
 
-request_result = requests.get(url)  # Request the url
-soup = BeautifulSoup(request_result.text, 'html.parser')
+    left_middle_box = soup.find_all('div', class_='aside-setting__chapter')
+    model_soup = left_middle_box[-1]
 
-left_top_box = soup.findAll('li', class_='aside-setting__views-item')
-publish_date = left_top_box[1].text.strip()
-publish_date_object = datetime.strptime(publish_date, "%Y-%m-%d")
+    model_list = []
+    found_model = model_soup.find_all('a', class_='aside-setting__models-link')
+    for model in found_model:
+        model_list.append(model.text.strip())
 
-left_middle_box = soup.find_all('div', class_='aside-setting__chapter')
-model_soup = left_middle_box[-1]
+    for model in model_list:
+        model_dir = resources_dir / model
 
-model_list = []
-found_model_soup = model_soup.find_all('a', class_='aside-setting__models-link')
-for potential_model in found_model_soup:
-    model_list.append(potential_model.text.strip())
+        if not model_dir.exists():
+            os.makedirs(model_dir)
 
-main_image_box = soup.find_all('div', class_='box-massage')[0]
-image_link_soup = main_image_box.find_all('a', class_='box-massage__card-link slideshowGalleryImage')
+    main_image_box = soup.find_all('div', class_='box-massage')[0]
+    image_link_soup = main_image_box.find_all('a', class_='box-massage__card-link slideshowGalleryImage')
 
-image_link_list = []
-for link in image_link_soup:
-    image_link_list.append(link.get('href'))
+    image_link_list = []
+    for link in image_link_soup:
+        image_link_list.append(link.get('href'))
 
-count = 0
-for image_link in image_link_list:
-    time.sleep(2.5)
+    image_link_list_length = len(image_link_list)
 
-    to_add_mod_time = publish_date_object + count * timedelta(seconds=1)
-    string_time = to_add_mod_time.strftime("%Y%m%d_%H%M%S")
+    count = 0
+    for image_link in image_link_list:
+        time.sleep(1.25)
 
-    parsed_url = urlparse(image_link).path.replace("/", '')
+        mod_time = publish_date_object + count * timedelta(seconds=1)
+        mod_time_string = mod_time.strftime("%Y%m%d_%H%M%S")
 
-    try:
-        image = Image.open(requests.get(image_link, stream=True).raw)
+        count += 1
+        print(f"{count}/{image_link_list_length} | Working: {image_link}")
 
-        for model in model_list:
+        parsed_url = urlparse(image_link).path.replace("/", '')
+        output_name = f"{mod_time_string}_{parsed_url}"
 
-            model_dir = beibusosu_resources_dir / model
+        try:
+            absent_condition = False
 
-            if not model_dir.exists():
-                os.makedirs(model_dir)
+            for model in model_list:
+                model_dir = resources_dir / model
+                image_path = model_dir / output_name
 
-            output_name = f"{model}_{string_time}_{parsed_url}"
-            image_path = model_dir / output_name
+                if not image_path.exists():
+                    absent_condition = True
 
-            if image.mode != 'RGB':
-                image = image.convert('RGB')
+            if absent_condition:
+                print("Some model copy missing")
+                image = Image.open(requests.get(image_link, stream=True).raw)
 
-            if not image_path.exists():
-                image.save(image_path, format='JPEG', quality=100)
+                if image.mode != 'RGB':
+                    image = image.convert('RGB')
+
+                for model in model_list:
+                    model_dir = resources_dir / model
+                    image_path = model_dir / output_name
+
+                    if not image_path.exists():
+                        image.save(image_path, format='JPEG', quality=100)
+                    else:
+                        print(f"{image_link} | Already downloaded for {model}")
+
+                    os.utime(image_path, (mod_time.timestamp(), mod_time.timestamp()))
 
             else:
-                print(f"Skipping")
+                print("All models involved cleared")
 
-            os.utime(image_path, (to_add_mod_time.timestamp(), to_add_mod_time.timestamp()))
+        except requests.exceptions.RequestException as error:
+            print(f"Cannot Fetch: {image_link}: {error}")
 
-    except requests.exceptions.RequestException as error:
-        print(f"Cannot Fetch: {image_link}: {error}")
+        except Exception as error:
+            print(f"Cannot Process: {image_link}: {error}")
 
-    except Exception as error:
-        print(f"Cannot Process: {image_link}: {error}")
 
-    count += 1
+if __name__ == "__main__":
+    script_path = Path(__file__).resolve()
+    project_dir = script_path.parent
+
+    print(f"Project directory: {project_dir}")
+
+    os.chdir(project_dir)
+
+    with open("beibusosu_resource_path.txt", "r") as beibusosu_resources_text:
+        beibusosu_resources_dir = Path(str(beibusosu_resources_text.readline()).replace('"', ''))
+
+    url = str(input("URL: "))
+
+    main_image_box_download(url, beibusosu_resources_dir)
