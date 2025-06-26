5c89dc68cc889c3b7566edad097a04f81db9ef45
alorsunique <sounique7800@gmail.com>
2025-06-26 23:30:24 +0800

    2025 June 26

--- Daily_Report/kpop_comeback_reader.py
+++ Daily_Report/kpop_comeback_reader.py
@@ -1,26 +1,11 @@
 # Reads the output file
 
 import time
-import os
 from datetime import datetime, timedelta
 from pathlib import Path
 
 import pandas as pd
-
-
-
 import yaml
-
-# Get the date for today, tomorrow, and the day after tomorrow
-def day_forward(date):
-    today = date
-    tomorrow = date + timedelta(days=1)
-    day_after_tomorrow = date + timedelta(days=2)
-
-    day_list = [today, tomorrow, day_after_tomorrow]
-
-    return day_list
-
 
 
 def find_project_root(script_path, marker):
@@ -37,9 +22,6 @@ def find_project_root(script_path, marker):
     return current_path
 
 
-
-
-
 def month_comeback(df, current_datetime_object, artist_list, reverse_month_map):
     # Get current month
     current_month = current_datetime_object.month
@@ -53,11 +35,6 @@ def month_comeback(df, current_datetime_object, artist_list, reverse_month_map):
     month_comeback_df = df.loc[mask_artist & mask_month]
 
     return month_comeback_df
-
-
-
-
-
 
 
 def remain_month_comeback(df, current_datetime_object, artist_list, reverse_month_map):
@@ -89,11 +66,10 @@ def remain_month_comeback(df, current_datetime_object, artist_list, reverse_mont
     mask_artist = df['Artist'].str.lower().isin(valid_artist_list)
     mask_month = df['Day'].isin(remaining_formatted_day_list)
 
+    # Filtered DF
     remain_month_comeback_df = df.loc[mask_artist & mask_month]
 
     return remain_month_comeback_df
-
-
 
 
 def next_day_comeback(df, current_datetime_object, artist_list, reverse_month_map, next_day_add):
@@ -110,18 +86,39 @@ def next_day_comeback(df, current_datetime_object, artist_list, reverse_month_ma
         formatted_day = f"{reverse_month_map[remaining_day.month]} {remaining_day.day}, {remaining_day.year}"
         remaining_formatted_day_list.append(formatted_day)
 
-    print(remaining_formatted_day_list)
     # Masks for artists and remaining days
     mask_artist = df['Artist'].str.lower().isin(artist_list)
     mask_month = df['Day'].isin(remaining_formatted_day_list)
 
+    # Filtered DF
     next_day_comeback_df = df.loc[mask_artist & mask_month]
 
-    print(next_day_comeback_df)
+    return next_day_comeback_df
 
-    for index, row in next_day_comeback_df.iterrows():
-        print(row)
 
+# Formats the dataframe to a string
+def format_output_string(period_df):
+    string_list = []
+
+    for index, row in period_df.iterrows():
+        comeback_day = row['Day']
+
+        if pd.isna(row['Time']):
+            comeback_time = "Not Specified"
+        else:
+            comeback_time = row['Time']
+
+        comeback_artist = row['Artist']
+        comeback_information = row['Relevant Info Site']
+
+        formatted_string = f"Day: {comeback_day} | Time: {comeback_time} | Artist: {comeback_artist} | Relevant Information: {comeback_information}"
+
+        string_list.append(formatted_string)
+
+    if len(string_list) == 0:
+        string_list.append("No relevant comeback in the next few days")
+
+    return string_list
 
 
 # Main
@@ -168,22 +165,12 @@ if __name__ == "__main__":
         next_year = current_year
 
     catalog_path = daily_report_dir / f"{current_year}_{str(current_month).zfill(2)}_KPop_Output.xlsx"
-
     df = pd.read_excel(catalog_path)
 
     catalog_path = daily_report_dir / f"{next_year}_{str(next_month).zfill(2)}_KPop_Output.xlsx"
-
     next_df = pd.read_excel(catalog_path)
 
     whole_df = pd.concat([df, next_df], ignore_index=True)
-
-    # Get tbe next three days
-    #projected_day_list = day_forward(current_datetime_object)
-
-    #print(projected_day_list)
-
-
-
 
     reverse_month_map = {
         1: "January",
@@ -200,89 +187,41 @@ if __name__ == "__main__":
         12: "December"
     }
 
-    month_comeback(whole_df, current_datetime_object, valid_artist_list, reverse_month_map)
+    month_comeback_df = month_comeback(whole_df, current_datetime_object, valid_artist_list, reverse_month_map)
 
-    remain_month_comeback(whole_df, current_datetime_object, valid_artist_list, reverse_month_map)
+    remain_month_comeback_df = remain_month_comeback(whole_df, current_datetime_object, valid_artist_list,
+                                                     reverse_month_map)
 
-    next_day_add = 5
-    next_day_comeback(whole_df, current_datetime_object, valid_artist_list, reverse_month_map,next_day_add)
+    next_day_add = 3
+    next_day_comeback_df = next_day_comeback(whole_df, current_datetime_object, valid_artist_list, reverse_month_map,
+                                             next_day_add)
 
-    time.sleep(1000)
-
-    day_string_list = []
-    for entry in projected_day_list:
-        formatted_day = f"{reverse_month_map[entry.month]} {entry.day}, {entry.year}"
-        day_string_list.append(formatted_day)
-
-    month_comeback_df = df.loc[(df['Artist'].str.lower().isin(valid_artist_list))]
-
-    print(month_comeback_df)
-
-    near_day_comeback_df = df.loc[
-        (df['Day'].isin(day_string_list))
-        & (df['Artist'].str.lower().isin(valid_artist_list))
-        ]
-
-    print(near_day_comeback_df)
-
-    string_list = []
-
-    for index, row in month_comeback_df.iterrows():
-        comeback_day = row['Day']
-
-        if pd.isna(row['Time']):
-            comeback_time = "Not Specified"
-        else:
-            comeback_time = row['Time']
-
-        comeback_artist = row['Artist']
-        comeback_information = row['Relevant Info Site']
-
-        formatted_string = f"Day: {comeback_day} | Time: {comeback_time} | Artist: {comeback_artist} | Relevant Information: {comeback_information}"
-
-        string_list.append(formatted_string)
-
-    if len(string_list) == 0:
-        string_list.append("No relevant comeback for the month")
-
-    print("Comeback for the month")
-    print(string_list)
+    # For the month
+    string_list = format_output_string(month_comeback_df)
 
     string_text = "month_comeback.txt"
     string_path = daily_report_dir / string_text
 
-    # Open the file in write mode and write the string to it
-    with open(string_path, 'w', encoding='utf-8') as month_text:
+    with open(string_path, 'w', encoding='utf-8') as comeback_text:
         for entry in string_list:
-            month_text.write(entry + '\n')
+            comeback_text.write(entry + '\n')
 
-    string_list = []
+    # For the remaining of the month
+    string_list = format_output_string(remain_month_comeback_df)
 
-    for index, row in near_day_comeback_df.iterrows():
-        comeback_day = row['Day']
+    string_text = "remain_month_comeback.txt"
+    string_path = daily_report_dir / string_text
 
-        if pd.isna(row['Time']):
-            comeback_time = "Not Specified"
-        else:
-            comeback_time = row['Time']
+    with open(string_path, 'w', encoding='utf-8') as comeback_text:
+        for entry in string_list:
+            comeback_text.write(entry + '\n')
 
-        comeback_artist = row['Artist']
-        comeback_information = row['Relevant Info Site']
-
-        formatted_string = f"Day: {comeback_day} | Time: {comeback_time} | Artist: {comeback_artist} | Relevant Information: {comeback_information}"
-
-        string_list.append(formatted_string)
-
-    if len(string_list) == 0:
-        string_list.append("No relevant comeback in the next few days")
-
-    print("Comeback for the next few days")
-    print(string_list)
+    # For the next few days
+    string_list = format_output_string(next_day_comeback_df)
 
     string_text = "next_day_comeback.txt"
     string_path = daily_report_dir / string_text
 
-    # Open the file in write mode and write the string to it
-    with open(string_path, 'w', encoding='utf-8') as next_day_text:
+    with open(string_path, 'w', encoding='utf-8') as comeback_text:
         for entry in string_list:
-            next_day_text.write(entry + '\n')
+            comeback_text.write(entry + '\n')
