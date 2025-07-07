7bd6ecaa59f6c0b6dc1f80fa8523b73288570c2e
alorsunique <sounique7800@gmail.com>
2025-07-07 16:08:45 +0800

    2025 July 07

--- Daily_Report/daily_orchestrate.py
+++ Daily_Report/daily_orchestrate.py
@@ -72,22 +72,28 @@ if __name__ == "__main__":
 
 
     def report_call():
-        kpop_comeback_tracker.main()
-        kpop_comeback_reader.main()
-        weather_test.main()
-        daily_report.main()
+        print('Report Call')
+        # kpop_comeback_tracker.main()
+        # kpop_comeback_reader.main()
+        # weather_test.main()
+        # daily_report.main()
+
+    # datetime_object = datetime(current_year,current_month,current_day,20)
 
 
-
-    count = 0
     index = 0
-    while count < len(formatted_run_time_list):
-        if datetime_object > formatted_run_time_list[count]:
+
+    for entry in formatted_run_time_list:
+        if datetime_object >= entry:
             index += 1
 
-        count +=1
 
-    index -= 1
+
+    print(index)
+
+    print(formatted_run_time_list)
+    print(formatted_run_time_list[index-1])
+
 
 
 
@@ -148,7 +154,7 @@ if __name__ == "__main__":
             # Here, the current hour should be rounded down
             # Rounding down is done in the index part
 
-            if run_time_list[index] > loaded_hour:
+            if run_time_list[index-1] > loaded_hour:
 
                 os.remove(daily_run_status_json_path)
 
@@ -156,7 +162,7 @@ if __name__ == "__main__":
                     'year': current_year,
                     'month': current_month,
                     'day': current_day,
-                    'time': run_time_list[index]
+                    'time': run_time_list[index-1]
                 }
                 with open(daily_run_status_json_path, 'w', encoding='utf-8') as output_json:
                     json.dump(run_status_dict, output_json)
@@ -164,28 +170,48 @@ if __name__ == "__main__":
                 report_call()
 
             else:
-                print(f"Run has been made for hour {run_time_list[index]}")
+                print(f"Run has been made for hour {run_time_list[index-1]}")
 
 
     # This is the part where it can loop since the checks have been made
 
-    # while True:
+    while True:
+        print(f'Index: {index}')
+        print(f'Current datetime object: {datetime_object}')
+        print(f'Next target time: {formatted_run_time_list[index]}')
 
-    print(index)
-    print(formatted_run_time_list[index])
-    print(datetime_object)
+        if index+1 == len(formatted_run_time_list):
+            print("Max hour for the day done. Next day list should be created")
 
-    if index+1 == len(formatted_run_time_list):
-        print("Max hour for the day done. Next day list should be created")
+            next_day_formatted_run_time_list = []
 
-        for entry in formatted_run_time_list:
-            print(entry)
-            print(entry+timedelta(days=1))
-    else:
+            for entry in formatted_run_time_list:
+                print(f'Formatted Entry: {entry}')
+                print(f'Incremented by a day {entry+timedelta(days=1)}')
+
+                next_day_entry = entry + timedelta(days=1)
+                next_day_formatted_run_time_list.append(next_day_entry)
+
+            formatted_run_time_list = next_day_formatted_run_time_list.copy()
+
+            print(formatted_run_time_list)
+
+            index = 0
 
 
-        datetime_object = datetime.fromtimestamp(time.time())
-        
+        to_sleep_time_object = (formatted_run_time_list[index]-datetime_object)
+        to_sleep_time = to_sleep_time_object.total_seconds()
+        print(f"Will sleep for {to_sleep_time} to reach {formatted_run_time_list[index]}")
+
+        datetime_object = datetime_object + to_sleep_time_object
+
+        report_call()
+
+
+        index += 1
+
+        time.sleep(1)
+
 
 
 
